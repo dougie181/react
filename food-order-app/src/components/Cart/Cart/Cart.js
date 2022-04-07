bab2ba8d43fdec15b3319033930e3749d5dd2a5b
dougie181 <doug@wwp.com.au>
2022-04-07 19:16:12 +1000

    Finished food order app

--- food-order-app/src/components/Cart/Cart/Cart.js
+++ food-order-app/src/components/Cart/Cart/Cart.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import Modal from "../../UI/Modal/Modal";
 import styles from "./Cart.module.css";
+import CartContext from "../../../store/cart-context";
+import CartItem from "../CartItem/CartItem";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = ((id) => {
+    cartCtx.removeItem(id);
+  });
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount: 1});
+  };
+
   const cartItems = (
     <ul className={styles["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: '2', price: '12.99' }].map((item) => {
-        return <li>{item.name}</li>;
+      {cartCtx.items.map((item) => {
+        return (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onAdd={cartItemAddHandler.bind(null,item)}
+            onRemove={cartItemRemoveHandler.bind(null,item.id)}
+          />
+        );
       })}
     </ul>
   );
@@ -16,12 +40,18 @@ const Cart = (props) => {
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
-        <span>35.63</span>
+        <span>{totalAmount}</span>
       </div>
 
       <div className={styles.actions}>
-        <button className={styles['button--alt']} onClick={props.onHide}>Close</button>
-        <button className={styles.button} onClick={props.onShow}>Order</button>
+        <button className={styles["button--alt"]} onClick={props.onHide}>
+          Close
+        </button>
+        {hasItems && (
+          <button className={styles.button} onClick={props.onShow}>
+            Order
+          </button>
+        )}
       </div>
     </Modal>
   );
--- food-order-app/src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,17 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../../store/cart-context";
 import CartIcon from "../../UI/CartIcon/CartIcon";
 
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cardCtx = useContext(CartContext);
-  const numberOfCartItems = cardCtx.items.reduce((currNumber, item) => { 
+  
+  const { items } = cardCtx;
+
+  const numberOfCartItems = items.reduce((currNumber, item) => { 
     return currNumber + item.amount;
   }, 0);
 
+  const btnStyles= `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    },300);
+
+    return (() => {
+      clearTimeout(timer);
+    });
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={props.onShow}>
+    <button className={btnStyles} onClick={props.onShow}>
     <span className={styles.icon}>
       <CartIcon />
     </span>
