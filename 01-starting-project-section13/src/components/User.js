074c75439e1becccff113d5ecb7a8278c243612c
Doug Inman <doug.inman@gmail.com>
2022-04-08 13:22:25 +1000

    Added User Finder Function Component

--- 01-starting-project-section13/src/components/User.js
+++ 01-starting-project-section13/src/components/User.js
@@ -3,6 +3,18 @@ import classes from './User.module.css';
 
 class User extends Component {
 
+  componentDidMount() {
+    //super();
+    console.log("componentDidMount");
+  }
+  
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
   }
