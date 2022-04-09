79fea9417457e28706ca115a43fd7cbec952c348
dougie181 <doug@wwp.com.au>
2022-04-09 21:12:07 +1000

    Added toggle and using switch for store

--- 01-starting-project-section18/src/store/index.js
+++ 01-starting-project-section18/src/store/index.js
@@ -1,13 +1,44 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
-    return { counter: state.counter + 1 };
+const initialState = {
+  counter: 0,
+  showCounter: true,
+};
+
+const counterReducer = (state = initialState, action) => {
+
+  switch (action.type) {
+    case "increment":
+      return {
+        counter: state.counter + 1,
+        showCounter: state.showCounter,
+      };
+      break;
+    case "increase":
+      return {
+        counter: state.counter + action.amount,
+        showCounter: state.showCounter,
+      };
+      break;
+    case "decrement":
+      return {
+        counter: state.counter - 1,
+        showCounter: state.showCounter,
+      };
+      break;
+    case "decrease":
+      return {
+        counter: state.counter - action.amount,
+        showCounter: state.showCounter,
+      };
+    case "toggle":
+      return {
+        counter: state.counter,
+        showCounter: !state.showCounter,
+      };
+    default:
+      return state;
   }
-  if (action.type === "decrement") {
-    return { counter: state.counter - 1 };
-  }
-  return state;
 };
 
 const store = createStore(counterReducer);
